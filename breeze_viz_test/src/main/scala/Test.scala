1c7ec7ffa7acefd78b4b7f175f30d22a74eba010
Dell-PC\Dell <sunxianpeng@xiaohulu.com>
2018-03-16 19:06:28 +0800

    update

---
+++ breeze_viz_test/src/main/scala/Test.scala
@@ -0,0 +1,17 @@
+import breeze.plot._
+import breeze.plot._
+/**
+  * \* Created with IntelliJ IDEA.
+  * \* User: sunxianpeng
+  * \* Date: 2018/3/15
+  * \* Time: 15:54
+  * \* To change this template use File | Settings | File Templates.
+  * \* Description: 
+  * \*/
+object Test {
+  def main(args: Array[String]): Unit = {
+
+
+  }
+}
+
